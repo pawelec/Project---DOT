cf872c6cf60f2a1630ef2c64c54bdab133fc617e
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-08 22:27:18 +0200

    Add constraint for primary key to entity model.

--- Domain/Models/Entity.cs
+++ Domain/Models/Entity.cs
@@ -3,6 +3,7 @@
 namespace Domain.Models
 {
     public class Entity<TKey>
+        where TKey : struct
     {
         public virtual TKey Id { get; set; }
         public virtual DateTime Created { get; set; }
