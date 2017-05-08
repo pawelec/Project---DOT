aecfb98d5f33adb20fff22bd28a8159b582d5211
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-08 23:02:14 +0200

    Add implementation of wishes repository.

---
+++ Core/Repositories/WishesRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Interfaces;
+using Domain.Models.Wishes;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Core.Contexts;
+using System.Linq;
+using Core.Extensions;
+
+namespace Core.Repositories
+{
+    public class WishesRepository : IRepository<int, Wish>
+    {
+        protected readonly Context _Context;
+
+        public WishesRepository()
+        {
+            this._Context = new Context();
+        }
+
+        public Wish Get(int key) => this._Context.Wishes.Find(key);
+
+        public ICollection<Wish> Get(Expression<Func<Wish, bool>> predicate)
+        {
+            if (predicate.IsNull())
+                return this._Context.Wishes.Where(predicate).ToList();
+            return new List<Wish>();
+        }
+
+        public ICollection<Wish> Get()
+            => this._Context.Wishes.ToList();
+    }
+}
