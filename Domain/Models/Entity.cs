06b70484a9dcf90d4bad8ecef982efad285bf70e
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-08 22:23:39 +0200

    Add creation date of entity.

--- Domain/Models/Entity.cs
+++ Domain/Models/Entity.cs
@@ -1,7 +1,10 @@
-﻿namespace Domain.Models
+﻿using System;
+
+namespace Domain.Models
 {
     public class Entity<TKey>
     {
         public virtual TKey Id { get; set; }
+        public virtual DateTime Created { get; set; }
     }
 }
