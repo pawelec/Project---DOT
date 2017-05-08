090d23f35cf7479691f56fbd43921f08f5aeddbb
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-08 22:30:07 +0200

    Add base generic repository interface with only getters.

---
+++ Domain/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository<TKey, TEntity>
+        where TKey : struct
+        where TEntity : Entity<TKey>
+    {
+        TEntity Get(TKey key);
+        ICollection<TEntity> Get(Expression<Func<TEntity, bool>> predicate);
+        ICollection<TEntity> Get();
+    }
+}
