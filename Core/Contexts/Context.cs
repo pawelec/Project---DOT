196378dfdea558e28a05e61aad5c7a4adcf8d631
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-08 22:51:51 +0200

    Move user entity to "Domain" project. Move application db context to "Core". And finally install necessary packages.

---
+++ Core/Contexts/Context.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models.Users;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Core.Contexts
+{
+    public class Context : IdentityDbContext<User>
+    {
+        public Context()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static Context Create()
+        {
+            return new Context();
+        }
+    }
+}
