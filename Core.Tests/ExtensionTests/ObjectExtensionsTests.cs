319ecb457b15d52f2053ebfbc01b4e1fd179934e
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-08 23:17:21 +0200

    Add unit tests for IsNull object extension method.

---
+++ Core.Tests/ExtensionTests/ObjectExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using Core.Extensions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Core.Tests.ExtensionTests
+{
+    public class ObjectExtensionsTests
+    {
+        [Fact]
+        public void IsNull_EntityIsNullReferenceType_ShouldReturnTrue()
+        {
+            // Arrange
+            object entity = null;
+
+            // Act
+            var actualResult = entity.IsNull();
+
+            // Assert
+            actualResult.Should().BeTrue();
+        }
+        [Fact]
+        public void IsNull_EntityIsNotNullReferenceType_ShouldReturnFalse()
+        {
+            // Arrange
+            object entity = new object();
+
+            // Act
+            var actualResult = entity.IsNull();
+
+            // Assert
+            actualResult.Should().BeFalse();
+        }
+        [Fact]
+        public void IsNull_NullableTypeAsNull_ShouldReturnTrue()
+        {
+            // Arrange
+            Nullable<int> entity = new Nullable<int>();
+
+            // Act
+            var actualResult = entity.IsNull();
+
+            // Assert
+            actualResult.Should().BeTrue();
+        }
+        [Fact]
+        public void IsNull_NullableTypeAsNotNull_ShouldReturnFalse()
+        {
+            // Arrange
+            Nullable<int> entity = 5;
+
+            // Act
+            var actualResult = entity.IsNull();
+
+            // Assert
+            actualResult.Should().BeFalse();
+        }
+        [Fact]
+        public void IsNull_ValueType_ShouldReturnFalse()
+        {
+            // Arrange
+            int entity = 5;
+
+            // Act
+            var actualResult = entity.IsNull();
+
+            // Assert
+            actualResult.Should().BeFalse();
+        }
+    }
+}
