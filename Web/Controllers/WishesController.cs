45da712ab11e97567b8d896d1a658711c60c2769
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-09 21:10:34 +0200

    Add Wishes controller and move newest wishes partial view to wishes views folder.

---
+++ Web/Controllers/WishesController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Repositories;
+using Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Web.Models.ViewModels.Wishes;
+
+namespace Web.Controllers
+{
+    public class WishesController : Controller
+    {
+        protected readonly IWishesRepository _wishesRepository;
+
+        public WishesController()
+        {
+            this._wishesRepository = new WishesRepository();
+        }
+
+        [AllowAnonymous]
+        public PartialViewResult GetNewestWishesPartial()
+        {
+            var newestWishes = AutoMapper.Mapper.Map<IEnumerable<WishViewModel>>(
+                this._wishesRepository.Get().OrderByDescending(wish => wish.Created).Take(3));
+            return PartialView("Partials/_NewestWishesPartial", newestWishes);
+        }
+    }
+}
