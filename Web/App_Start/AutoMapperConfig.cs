c07889998881e0850616e559ce0f242c66e951de
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-09 20:51:58 +0200

    Add AutoMapper config to app start.

---
+++ Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace Web
+{
+    public class AutoMapperConfig
+    {
+        public static void ConfigureMappings()
+        {
+            AutoMapper.Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Domain.Models.Wishes.Wish, Web.Models.ViewModels.Wishes.WishViewModel>();
+            });
+        }
+    }
+}
