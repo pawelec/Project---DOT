1e7b3c48895cf68fb7d0e5b6225036d0850ec446
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-09 21:59:10 +0200

    Add custom validation attribute.

---
+++ Web/Attributes/StringNullOrWhitespaceValidationAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Attributes
+{
+    public class StringNullOrWhitespaceValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string str = value?.ToString();
+            if (string.IsNullOrWhiteSpace(str))
+                return new ValidationResult("Wartość musi zostać uzupełniona.");
+            return ValidationResult.Success;
+        }
+    }
+}
--- Web/Models/ViewModels/Wishes/CreateWishViewModel.cs
+++ Web/Models/ViewModels/Wishes/CreateWishViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using Web.Attributes;
 
 namespace Web.Models.ViewModels.Wishes
 {
     public class CreateWishViewModel
     {
+        [StringNullOrWhitespaceValidation]
         public string Content { get; set; }
         public DateTime Created { get; set; } = DateTime.UtcNow;
     }
