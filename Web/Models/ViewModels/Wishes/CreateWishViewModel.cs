07a92bff496a77d366593faf04945253b6f27b43
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-14 13:06:36 +0200

    Fix create new wish functionality.

--- Web/Models/ViewModels/Wishes/CreateWishViewModel.cs
+++ Web/Models/ViewModels/Wishes/CreateWishViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using Web.Attributes;
 
 namespace Web.Models.ViewModels.Wishes
 {
     public class CreateWishViewModel
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Treść jest wymagana!")]
         [StringNullOrWhitespaceValidation]
         public string Content { get; set; }
+        public DateTime Created = DateTime.UtcNow;
     }
 }
