49fa5b30ea04db3605258109ce54da9f53689681
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-14 11:39:42 +0200

    Add link to wish from comments partial box.

--- Web/App_Start/AutoMapperConfig.cs
+++ Web/App_Start/AutoMapperConfig.cs
@@ -9,7 +9,8 @@
                 cfg.CreateMap<Domain.Models.Wishes.Wish, Web.Models.ViewModels.Wishes.WishViewModel>()
                     .ForMember(dst => dst.Creator, opt => opt.MapFrom(src => src.Creator.UserName));
                 cfg.CreateMap<Domain.Models.Comments.Comment, Web.Models.ViewModels.Comments.CommentViewModel>()
-                    .ForMember(dst => dst.Creator, opt => opt.MapFrom(src => src.Creator));
+                    .ForMember(dst => dst.Creator, opt => opt.MapFrom(src => src.Creator))
+                    .ForMember(dst => dst.WishId, opt => opt.MapFrom(src => src.Wish.Id));
 
                 cfg.CreateMap<Web.Models.ViewModels.Wishes.CreateWishViewModel, Domain.Models.Wishes.Wish>();
                 cfg.CreateMap<Web.Models.ViewModels.Comments.CreateCommentViewModel, Domain.Models.Comments.Comment>();
--- Web/Models/ViewModels/Comments/CommentViewModel.cs
+++ Web/Models/ViewModels/Comments/CommentViewModel.cs
@@ -3,5 +3,6 @@
     public class CommentViewModel : BaseViewModelEntity<int>
     {
         public string Content { get; set; }
+        public int WishId { get; set; }
     }
 }
