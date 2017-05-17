e1873e6a2bd627be3625e186628887e5af5c076a
Paweł Pawelec <Pawelec.Pawel@outlook.com>
2017-05-17 18:34:45 +0200

    Add count number of wishes and comments for admin control page.

--- Web/Controllers/AdminController.cs
+++ Web/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Contexts;
+using Core.Repositories;
+using Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,20 @@ namespace Web.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        private readonly IWishesRepository _wishesRepository;
+        private readonly ICommentsRepository _commentsRepository;
+
+        public AdminController()
+        {
+            var cxt = new Context();
+            this._wishesRepository = new WishesRepository(cxt);
+            this._commentsRepository = new CommentsRepository(cxt);
+        }
         // GET: Admin
         public ActionResult Index()
         {
+            ViewBag.Wishes = this._wishesRepository.Get().Count;
+            ViewBag.Comments = this._commentsRepository.Get().Count;
             return View();
         }
     }
